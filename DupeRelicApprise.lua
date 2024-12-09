4514c2af3bfc3f56277d6b079e6f7b42ae4b63b3
ARBIX <youssefalsadick@gmail.com>
2024-12-09 20:54:51 +0300

    Update DupeRelicApprise.lua

--- DupeRelicApprise.lua
+++ DupeRelicApprise.lua
@@ -1,3 +1,47 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
+
+MakeWindow({
+  Hub = {
+    Title = "ARBIX HUB | FISCH",
+    Animation = "by : TOUKA"
+  },
+  Key = {
+    KeySystem = false,
+    Title = "Key System",
+    Description = "",
+    KeyLink = "",
+    Keys = {"1234"},
+    Notifi = {
+      Notifications = true,
+      CorrectKey = "Running the Script...",
+      Incorrectkey = "The key is incorrect",
+      CopyKeyLink = "Copied to Clipboard"
+    }
+  }
+})
+
+
+MinimizeButton({
+  Image = "",
+  Size = {40, 40},
+  Color = Color3.fromRGB(10, 10, 10),
+  Corner = true,
+  Stroke = true,
+  StrokeColor = Color3.fromRGB(255, 0, 0)
+})
+
+local Main = MakeTab({Name = "Main"})
+
+
+
+MakeNotifi({
+  Title = "ARBIX HUB",
+  Text = "JOIN OUR TELEGRAM",
+  Time = 5
+})
+
+local section = AddSection(Main, {"Dupe"})
+
 
 
 
