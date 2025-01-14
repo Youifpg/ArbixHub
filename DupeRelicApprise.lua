468ff163143bd61eaa2ca776f293c5af7afd88c8
ARBIX <youssefalsadick@gmail.com>
2025-01-14 12:48:58 +0300

    Update DupeRelicApprise.lua

--- DupeRelicApprise.lua
+++ DupeRelicApprise.lua
@@ -38,8 +38,14 @@ MakeNotifi({
 
 local Main = MakeTab({Name = "INFO"})
 
+local Image = AddImageLabel(Main, {
+  Name = "",
+  Image = "rbxassetid://126511980185658"
+})
+
 AddTextLabel(Main, "This is TOUKA x UGPHONE script if you want inf money farm for 24/7 offline go discord")
 AddTextLabel(Main, "discord : https://discord.gg/VRHYbyj8Dy")
+
 local Main = MakeTab({Name = "SERVERS"})
 local section = AddSection(Main, {"press button to find old server"})
 local Main = MakeTab({Name = "MONEY"})
